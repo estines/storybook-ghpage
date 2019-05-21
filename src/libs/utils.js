3033da9150be4cacb6abd1c1968ddf17fdac4e47
Dszix <dszix@Dszixs-MacBook-Pro.local>
2019-05-21 17:24:20 +0700

    prepare to deploy on gh-pages

--- src/libs/utils.js
+++
@@ -1,9 +0,0 @@
-exports.uniqString = async array => {
-  let returnArray = []
-  for (let data of array) {
-    if (!returnArray.includes(data)) {
-      returnArray.push(data)
-    }
-  }
-  return returnArray
-}
--- storybook/stories/index.js
+++
@@ -1,4 +0,0 @@
-export * from './layout'
-export * from './Text'
-export * from './Input'
-export * from './button'
